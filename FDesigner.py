bceba57757752a33d8c8ee1a7feefbc93aed4f71
Gwerr1002 <98936739+Gwerr1002@users.noreply.github.com>
2022-03-04 19:36:40 -0600

    Update FDesigner.py

--- FDesigner.py
+++ FDesigner.py
@@ -4,6 +4,11 @@
 Created on Thu Mar  3 12:28:51 2022
 
 @author: Gerardo Ortíz Montufar
+
+This work is licensed under the Creative Commons Attribution 4.0
+International License. To view a copy of this license, visit
+http://creativecommons.org/licenses/by/4.0/ or send a letter to
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 """
 
 from numpy import pi
